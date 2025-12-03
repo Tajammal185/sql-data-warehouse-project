393cb68e01dc7bca691ea08e1ae0f035399ffc7b
Tajammal185 <108747577+Tajammal185@users.noreply.github.com>
2025-12-03 23:32:06 +0500

    Create DDL script for Silver tables
    This script creates all required tables within the 'silver' schema, dropping existing ones if necessary.

---
+++ script/silver/ddl_silver.sql
@@ -0,0 +1,10 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Purpose:
+    This script creates all required tables within the 'silver' schema. If any of
+    these tables already exist, they will be dropped and recreated.
+    Run this script to reset and redefine the DDL structure of the Silver tables.
+===============================================================================
+*/
